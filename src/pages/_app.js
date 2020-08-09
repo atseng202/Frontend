b4c3760444383e0b8b9e6bb0088d78f6772aff2e
ethull <ethull.protonmail.com>
2020-08-09 16:14:16 +0100

    removed bootstrap code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,4 @@
 import "../styles/global.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "font-awesome/css/font-awesome.min.css";
-//import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-
-
-import '../styles.css'
 import 'antd/dist/antd.css';
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
